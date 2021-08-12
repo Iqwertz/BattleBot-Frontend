745961b556d3d266ed2484487631569bf5f096df
Julius <juliushussl@gmail.com>
2021-08-13 00:39:54 +0200

    console typing animation

--- src/app/services/console.service.ts
+++ src/app/services/console.service.ts
@@ -9,16 +9,47 @@ export class ConsoleService {
     this.print('Bot Console')
     this.print('-v 1.0.0')
     this.print('The Output of the compiler and the simulation get printed here')
-    this.print('')
+    this.print(' ')
   }
 
-  consoleData: string[] = [];
+  consoleData: string[] = [''];
+
+  private consoleDataTemp: string[] = [];
+  private isPrinting: boolean = false;
 
   clear() {
-    this.consoleData = []
+    this.consoleData = [];
+    this.consoleDataTemp = []
   }
 
   print(msg: string) {
-    this.consoleData.push(msg);
+    this.consoleDataTemp.push(msg);
+    console.log(this.isPrinting)
+    if (!this.isPrinting) {
+      this.printWithDelay();
+    }
+  }
+
+  private printWithDelay() {
+    console.log(this.consoleDataTemp.length)
+    if (this.consoleDataTemp.length > 0) {
+      this.isPrinting = true;
+      let char = this.consoleDataTemp[0].charAt(0);
+      this.consoleData[this.consoleData.length - 1] += char
+      this.consoleDataTemp[0] = this.consoleDataTemp[0].substr(1);
+      if (this.consoleDataTemp[0].length == 0) {
+        this.consoleDataTemp.shift();
+        this.consoleData.push('')
+      }
+      if (this.consoleDataTemp.length > 0) {
+        setTimeout(() => { this.printWithDelay() }, 10)
+      } else {
+        this.isPrinting = false;
+      }
+
+    } else {
+      console.log("sadad")
+      this.isPrinting = false;
+    }
   }
 }
