27b5dcde3cf5bd26f150b455da34ac63fd6efc05
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2021-08-13 11:45:51 +0200

    terminals saved to localStorage

--- src/app/services/terminals.service.ts
+++ src/app/services/terminals.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Command } from '../components/editor-ide/editor-ide.component';
 import { BrainFunctions } from './bot-compiler.service';
+import { Select } from '@ngxs/store';
+import { AppState } from '../store/app.state';
+import { Bot } from '../components/battle-map/battle-map.component';
+import { toString } from 'lodash';
 
 export interface Terminal {
   commands: Command[];
@@ -11,22 +15,57 @@ export interface Terminal {
   providedIn: 'root',
 })
 export class TerminalsService {
+  @Select(AppState.compiledBot) compiledBot$: any;
+
   terminals: Map<BrainFunctions, Terminal> = new Map();
 
   constructor() {
-    this.terminals.set('default', {
-      commands: [],
-      allowLogic: false,
-    });
+    let savedTerminals = localStorage.getItem('lastTerminal');
+    if (savedTerminals) {
+      this.terminals = JSON.parse(savedTerminals, this.reviver);
+    } else {
+      this.terminals.set('default', {
+        commands: [],
+        allowLogic: false,
+      });
 
-    this.terminals.set('onWallDetected', {
-      commands: [],
-      allowLogic: true,
-    });
+      this.terminals.set('onWallDetected', {
+        commands: [],
+        allowLogic: true,
+      });
 
-    this.terminals.set('onTrackDetected', {
-      commands: [],
-      allowLogic: true,
+      this.terminals.set('onTrackDetected', {
+        commands: [],
+        allowLogic: true,
+      });
+    }
+
+    this.compiledBot$.subscribe((b: Bot | undefined) => {
+      localStorage.setItem(
+        'lastTerminal',
+        JSON.stringify(this.terminals, this.replacer)
+      );
     });
+  }
+
+  /////From: https://stackoverflow.com/questions/29085197/how-do-you-json-stringify-an-es6-map
+  private replacer(key: string, value: any) {
+    if (value instanceof Map) {
+      return {
+        dataType: 'Map',
+        value: Array.from(value.entries()), // or with spread: value: [...value]
+      };
+    } else {
+      return value;
+    }
+  }
+
+  private reviver(key: string, value: any) {
+    if (typeof value === 'object' && value !== null) {
+      if (value.dataType === 'Map') {
+        return new Map(value.value);
+      }
+    }
+    return value;
   }
 }
